04fbc2125fb66140c2f080995f4d2931779f5f2a
Miguel Rodrigues <miguel.rodrigues8@hotmail.com>
2024-06-14 11:22:09 +0100

    feat: add emptyState

--- coding-challenge/src/components/empty-state/EmptyState.tsx
+++ coding-challenge/src/components/empty-state/EmptyState.tsx
@@ -2,10 +2,10 @@ import NoDataFound from "../../theme/illustrations";
 
 const EmptyState = () => {
   return (
-    <p className="flex flex-col items-center justify-center gap-2">
+    <section className="flex flex-col items-center justify-center gap-2">
       <NoDataFound />
       <h2 className="font-bold text-2xl mt-[-50px]">No results found.</h2>
-    </p>
+    </section>
   );
 };
 
