b150cb33fd4e900a449745d7c4fecf2b368adae7
Miguel Rodrigues <miguel.rodrigues8@hotmail.com>
2024-06-19 09:07:51 +0100

    fix: turn emptyState button into NavLink

--- coding-challenge/src/pages/NotFoundPage.tsx
+++ coding-challenge/src/pages/NotFoundPage.tsx
@@ -1,12 +1,16 @@
+import { NavLink } from "react-router-dom";
 import EmptyState from "../components/empty-state/EmptyState";
 
 const NotFoundPage = () => {
   return (
     <section className="flex flex-col gap-8 justify-center items-center">
       <EmptyState />
-      <button className="flex text-white mx-2 gap-4 text-center items-center justify-center text-lg font-bold rounded-full w-36 p-3 pl-4 hover:bg-blue-700 bg-blue-500 transition duration-300 ease-in-out">
+      <NavLink
+        to="/"
+        className="flex text-white mx-2 gap-4 text-center items-center justify-center text-lg font-bold rounded-full w-36 p-3 pl-4 hover:bg-blue-700 bg-blue-500 transition duration-300 ease-in-out"
+      >
         Go Back
-      </button>
+      </NavLink>
     </section>
   );
 };
